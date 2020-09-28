73d376d6c1b208ad99cee2e69ee5019a1e8c7ea7
muramoto ken-ichi <muramoto@giftee.co>
2020-09-28 18:28:25 +0900

    update README.md

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
 const core = require('@actions/core');
 const aws = require('aws-sdk');
 
-const TASK_DEFINITION_ARN_PATTERN = /^arn:aws:ecs:(?<region>.+?):(?<account_id>[0-9]+?):task-definition\/(?<cluster>.+?):[0-9]+?$/
-const MAX_LOOP = 10
+const TASK_DEFINITION_ARN_PATTERN = /^arn:aws:ecs:(?<region>.+?):(?<account_id>[0-9]+?):task-definition\/(?<task_definition_name>.+?):[0-9]+?$/
+const LIST_RULE_NAMES_BY_TARGET_API_MAX_LOOP = 10
+const FIRST = 0
 
 async function run() {
     try {
-        const updateTaskDefinitionArn = core.getInput('update-task-definition-arn', { required: true });
-        const cluster = core.getInput('cluster', { required: false });
-
         const config = new aws.Config()
         const sts = new aws.STS({apiVersion: '2011-06-15'});
         const cloudWatchEvent = new aws.CloudWatchEvents({apiVersion: '2015-10-07'});
 
-        const getCallerIdentityResponse = await sts.getCallerIdentity({}).promise();
-        core.debug(getCallerIdentityResponse);
+        // get input
+        const updateTaskDefinitionArn = core.getInput('update-task-definition-arn', { required: true });
+        const cluster = core.getInput('cluster', { required: false });
 
-        for (let i = 0, nextToken = ""; i < MAX_LOOP; i++) {
-            const parameter = {TargetArn: "arn:aws:ecs:" + config.region + ":" + getCallerIdentityResponse.Account + ":cluster/" + cluster}
+        // get AccountID
+        const getCallerIdentityResponse = await sts.getCallerIdentity({}).promise();
+        core.debug('getCallerIdentityResponse - ' + JSON.stringify(getCallerIdentityResponse));
+
+        for (let i = 0, nextToken = ""; i < LIST_RULE_NAMES_BY_TARGET_API_MAX_LOOP; i++) {
+            // get ECS Task Schedule(CloudWatchEvent Rule) list
+            const listRuleNamesByTargetParameter = {TargetArn: "arn:aws:ecs:" + config.region + ":" + getCallerIdentityResponse.Account + ":cluster/" + cluster}
 
             if (nextToken) {
-                parameter["NextToken"] = nextToken
+                listRuleNamesByTargetParameter["NextToken"] = nextToken
             }
 
-            const listRuleNamesByTargetResponse = await cloudWatchEvent.listRuleNamesByTarget(parameter).promise();
-            core.debug(listRuleNamesByTargetResponse);
+            core.debug('listRuleNamesByTargetParameter - ' + JSON.stringify(listRuleNamesByTargetParameter));
+            const listRuleNamesByTargetResponse = await cloudWatchEvent.listRuleNamesByTarget(listRuleNamesByTargetParameter).promise();
+            core.debug('listRuleNamesByTargetResponse - ' + JSON.stringify(listRuleNamesByTargetResponse));
 
             for (let i = 0; i < listRuleNamesByTargetResponse.RuleNames.length; i++) {
-                const ruleName = listRuleNamesByTargetResponse.RuleNames[i]
-                const listTargetsByRuleResponse = await cloudWatchEvent.listTargetsByRule({Rule: ruleName}).promise();
-                core.debug(listTargetsByRuleResponse);
+                // get ECS Task Schedule(CloudWatchEvent Rule) detail
+                const listTargetsByRuleParameter ={
+                    Rule: listRuleNamesByTargetResponse.RuleNames[i]
+                }
 
-                const taskDefinitionArn = listTargetsByRuleResponse.Targets[0].EcsParameters.TaskDefinitionArn
-                const taskDefinitionArnMatch = taskDefinitionArn.match(TASK_DEFINITION_ARN_PATTERN)
+                core.debug('listTargetsByRuleParameter - ' + JSON.stringify(listTargetsByRuleParameter));
+                const listTargetsByRuleResponse = await cloudWatchEvent.listTargetsByRule(listTargetsByRuleParameter).promise();
+                core.debug('listTargetsByRuleResponse - ' + JSON.stringify(listTargetsByRuleResponse));
+
+                // update Task Definition Arn for ECS Task Schedule(CloudWatchEvent Rule)
+                const currentTaskDefinitionArn = listTargetsByRuleResponse.Targets[0].EcsParameters.TaskDefinitionArn
+                const currentTaskDefinitionArnMatch = currentTaskDefinitionArn.match(TASK_DEFINITION_ARN_PATTERN)
                 const updateTaskDefinitionArnMatch = updateTaskDefinitionArn.match(TASK_DEFINITION_ARN_PATTERN)
 
-                if (JSON.stringify(taskDefinitionArnMatch.groups) != JSON.stringify(updateTaskDefinitionArnMatch.groups)) {
+                if (JSON.stringify(currentTaskDefinitionArnMatch.groups) != JSON.stringify(updateTaskDefinitionArnMatch.groups)) {
+                    // skip not equal task definition name
                     continue;
                 }
 
-                const putTargetParameter = {Rule: ruleName, Targets: listTargetsByRuleResponse.Targets}
-                putTargetParameter.Targets[0].EcsParameters.TaskDefinitionArn = updateTaskDefinitionArn
+                const putTargetParameter = {
+                    Rule: listRuleNamesByTargetResponse.RuleNames[i],
+                    Targets: listTargetsByRuleResponse.Targets
+                }
 
+                putTargetParameter.Targets[FIRST].EcsParameters.TaskDefinitionArn = updateTaskDefinitionArn
+
+                core.debug('putTargetParameter - ' + JSON.stringify(putTargetParameter));
                 const putTargetsResponse = await cloudWatchEvent.putTargets(putTargetParameter).promise();
-                core.debug(putTargetsResponse);
+                core.debug('putTargetsResponse - ' + JSON.stringify(putTargetsResponse));
+
+                if (putTargetsResponse.FailedEntries.length > 0) {
+                    const error_messages = putTargetsResponse.FailedEntries.map(failedEntry => failedEntry.ErrorMessage);
+                    throw new Error(error_messages.join("\n"));
+                }
             }
 
             nextToken = listRuleNamesByTargetResponse.NextToken
@@ -54,7 +76,8 @@ async function run() {
         }
 
     } catch (error) {
-        core.debug(error);
+        core.setFailed(error.message);
+        core.debug(error.stack);
     }
 }
 
